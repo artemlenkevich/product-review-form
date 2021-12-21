bc34d09b49a7e5904789e2723096d8c27cc2ca8d
Artem Lenkevich <ar.lenkevich@gmail.com>
2021-12-21 16:31:25 +0300

    HOC Formik

--- src/ProductReviewForm/ProductReviewForm.tsx
+++ src/ProductReviewForm/ProductReviewForm.tsx
@@ -1,5 +1,5 @@
 import styles from './ProductReviewForm.module.scss';
-import { Formik, Form, Field, useFormik } from 'formik';
+import { Formik, Form, Field } from 'formik';
 
 interface FormValues {
     fullName: string;
@@ -21,34 +21,26 @@ const validate = (values: FormValues) => {
 
 export const ProductReviewForm: React.FC<{}> = () => {
 
-    const formik = useFormik({
-        initialValues: {
-            fullName: '',
-            email: '',
-            review: ''
-        },
-        validate,
-        onSubmit: (values: FormValues) => {
-            console.log(values);
-        }
-    })
-
+    
     return (
-        <form onSubmit={formik.handleSubmit}>
-            <div className={styles['input-group']}>
-                { formik.errors.fullName ? <div>{formik.errors.fullName}</div> : <label htmlFor='fullName'>ФИО</label> }
-                <input 
-                    id='fullName'
-                    name='fullName'
-                    type='text' 
-                    className={styles.input} 
-                    onChange={formik.handleChange} 
-                    value={formik.values.fullName} 
-                />
-            </div>
-            <input className={styles.input} type='text' name='email'/>
-            <textarea className={styles.textarea} name='review'/>
-            <button type='submit'>Submit</button>
-        </form>
+        <Formik
+            initialValues={{ fullName: '', email: '', review: ''}}
+            onSubmit={(values: FormValues) => {
+                console.log(values);
+            }}
+            validate={validate}
+        >
+            {({ errors, touched, isValidating, handleChange, values }) => (
+                <Form>
+                    <div className={styles['input-group']}>
+                        { errors.fullName ? <div>{errors.fullName}</div> : <label htmlFor='fullName'>ФИО</label> }
+                        <Field className={styles.input} name='fullName' id='fullName' type='text'/>
+                    </div>
+                    <Field className={styles.input} type='text' name='email'/>
+                    <textarea className={styles.textarea} name='review'/>
+                    <button type='submit'>Submit</button>
+                </Form>
+            )}            
+        </Formik>
     )
 }
